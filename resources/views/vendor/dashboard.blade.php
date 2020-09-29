af19fb9f203d191d1bd98dd594f51c849bb26c72
chhatraman shrestha <cms111000111@gmail.com>
2020-09-30 00:47:45 +0545

    sdf

--- resources/views/vendor/dashboard.blade.php
+++ resources/views/vendor/dashboard.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.sellerlayouts.seller-design')
 @section('content')
+@if($data->verified==0)
+        <div class="container">
+            <div class="card">
+                <div class="card-header card-header-danger">
+                    <h4 class="card-title">Full header coloured</h4>
+                    <p class="category">Category subtitle</p>
+                </div>
+                <div class="card-body">
+                      The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona...
+                </div>
+            </div>
+          </div>
+        </div>
 
+    @endif
 @endsection
