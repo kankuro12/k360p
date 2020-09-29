58370567959167ce0760b3c2ae349ca6357ed26a
chhatraman shrestha <cms111000111@gmail.com>
2020-09-30 00:57:25 +0545

    sdf

--- resources/views/vendor/dashboard.blade.php
+++ resources/views/vendor/dashboard.blade.php
@@ -1,17 +1,32 @@
 @extends('layouts.sellerlayouts.seller-design')
 @section('content')
-@if($data->verified==0)
-        <div class="container">
-            <div class="card">
-                <div class="card-header card-header-danger">
-                    <h4 class="card-title">Full header coloured</h4>
-                    <p class="category">Category subtitle</p>
-                </div>
-                <div class="card-body">
-                      The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona...
+    @if ($data->verified == 0)
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Regular header</h4>
+                        <p class="category">Category subtitle</p>
+                    </div>
+                    <div class="card-body">
+                        The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where
+                        you can enjoy the main night life in Barcelona...
+                    </div>
                 </div>
             </div>
-          </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header card-header-danger">
+                        <h4 class="card-title">Full header coloured</h4>
+                        <p class="category">Category subtitle</p>
+                    </div>
+                    <div class="card-body">
+                        The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where
+                        you can enjoy the main night life in Barcelona...
+                    </div>
+                </div>
+            </div>
         </div>
 
     @endif
