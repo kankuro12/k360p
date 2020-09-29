97e86ce5e5240ba8d581b2bc448bd861490dad66
chhatraman shrestha <cms111000111@gmail.com>
2020-09-29 23:25:10 +0545

    as

--- app/Http/Controllers/Vendor/Auth/LoginController.php
+++ app/Http/Controllers/Vendor/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\vendor\Auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use App\User;
 use Session;
