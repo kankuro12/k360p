5ce80952ca0f45986dec8e0eb5cb50ae9fe4b8f2
chhatraman shrestha <cms111000111@gmail.com>
2020-10-02 21:48:14 +0545

    delivery type added

---
+++ database/migrations/2020_10_02_160003_add_delivery_to_order_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDeliveryToOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            //
+            $table->integer('deliverytype')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->dropColumn('deliverytype')->default(0);
+        });
+    }
+}
