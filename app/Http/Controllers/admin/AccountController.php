033395cc21fd1e28a65593e21f85fcafb010ec37
chhatraman shrestha <cms111000111@gmail.com>
2020-10-14 23:59:50 +0545

    new withdrawl

--- app/Http/Controllers/admin/AccountController.php
+++ app/Http/Controllers/admin/AccountController.php
@@ -26,23 +26,37 @@ class AccountController extends Controller
         return view('admin.account.withdrawl',compact('account','attributes'));
     }
 
+    public function withdrawl1($id,Request $request){
+        $account=new VendorAccount($id);
+       
+        $all=$request->all();
+        // dd($all);
+        $attributes=explode(',',env('withdrawldetails',"bank,account"));
+        if(!in_array("Order_Date",$attributes)){
+            array_push($attributes,"Order_Date");
+        }
+        return view('admin.account.withdrawl1',compact('account','attributes','all'));
+    }
     public function list($id){
 
     }
     public function detail($id){
+        $attributes=explode(',',env('withdrawldetails',"bank,account"));
+        $account=new VendorAccount($id);
+        $withdrawls=VendorWithdrawl::where('vendor_id',$id)->get();
         if(env('paymentstyle',0)==0){
             
-            $withdrawls=VendorWithdrawl::where('vendor_id',$id)->get();
-            $attributes=explode(',',env('withdrawldetails',"bank,account"));
-            $account=new VendorAccount($id);
             return view('admin.account.detail',compact('withdrawls','attributes','id','account'));
         }else{
             
+            if(!in_array("Order_Date",$attributes)){
+                array_push($attributes,"Order_Date");
+            }
             $data=OrderPayment::where('paid',0)-> get()->groupBy(function($date) {
                 return \Carbon\Carbon::parse($date->created_at)->toDateString();
             });
            
-            return view('admin.account.detail1',compact('data','attributes','id','account'));
+            return view('admin.account.detail1',compact('data','attributes','id','account','withdrawls'));
         }
     }
 
@@ -75,5 +89,43 @@ class AccountController extends Controller
 
         return redirect()->route('admin.detail',['id'=>$id]);
 
+    }
+
+    public function saveWithdrawl1($id,Request $request){
+        // dd($request);
+
+        $all=$request->all();
+        $attributes=explode(',',env('withdrawldetails',"bank,account"));
+        if(!in_array("Order_Date",$attributes)){
+            array_push($attributes,"Order_Date");
+        }
+        foreach ($attributes as $attribute) {
+            $arr[$attribute]=$request[$attribute];
+        }
+        $withdrawl=new VendorWithdrawl();
+        $withdrawl->amount=$request->amount;
+        $withdrawl->paymentdetails=$arr;
+        $withdrawl->stage=2;
+        $date=Carbon::now();
+        $withdrawl->requesteddate=$date;
+        $withdrawl->accecpteddate=$date;
+        $withdrawl->completeddate=$date;
+        $withdrawl->vendor_id=$id;
+        $withdrawl->image=$request->file('image')->store('images/withdrawl');
+        $withdrawl->save();
+
+        // $withdrawl->date=$date;
+
+
+        OrderPayment::whereIn('id',$request->ids)
+        ->update(['paid'=>1]);
+
+      
+
+        $account=AdminVendorAccount::where('vendor_id',$id)->first();
+        $account->amount-=$request->amount;
+        $account->save();
+
+        return redirect()->route('admin.detail',['id'=>$id]);
     }
 }
