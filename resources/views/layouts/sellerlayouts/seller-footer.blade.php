c1a63d67d2214fd271c4b53e62f349052e93b6d3
chhatraman shrestha <cms111000111@gmail.com>
2020-10-17 12:04:49 +0545

    footer changed

--- resources/views/layouts/sellerlayouts/seller-footer.blade.php
+++ resources/views/layouts/sellerlayouts/seller-footer.blade.php
@@ -8,11 +8,11 @@
             </ul>
         </nav>
         <p class="copyright pull-right">
-            &copy;
+           Crafted by
             <script>
                 document.write(new Date().getFullYear())
             </script>
-            <a href="https://www.needtechnosoft.com.np"> Need Technosoft </a>, made with love for a better Shopping Experience
+             <a href="https://www.needtechnosoft.com.np"> Need Technosoft </a>, For {{env('APP_NAME','LARAVEL')}}
         </p>
     </div>
 </footer>
