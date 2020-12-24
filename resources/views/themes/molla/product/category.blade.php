24e933c1f746225ba3ad4e41c08ea8636ee6fbe5
Gopalghimire1 <32799706+Gopalghimire1@users.noreply.github.com>
2020-12-24 10:40:43 +0545

    update category

--- resources/views/themes/molla/product/category.blade.php
+++ resources/views/themes/molla/product/category.blade.php
@@ -1,5 +1,8 @@
 @extends('themes.molla.layouts.app')
 @section('title','Category')
+@section('css')
+<link rel="stylesheet" href="{{ asset('themes\molla\assets\css\plugins\nouislider\nouislider.css') }}">
+@endsection
 @section('contant')
 @php
     $isgrid=1;
@@ -112,4 +115,34 @@
             </div><!-- End .page-content -->
 </main><!-- End .main -->
 
+@endsection
+@section('js')
+
+<script src="{{ asset("themes/molla/assets/js/nouislider.min.js") }}"></script>
+<script>
+    var slider = document.getElementById('need-price-slider');
+    noUiSlider.create(slider, {
+            start: [{{$_min}}, {{$_max}}],
+            connect: true,
+            step: 50,
+            margin: 200,
+            range: {
+                min: {{$min}},
+                max: {{$max}},
+            },
+            tooltips: true,
+            format: wNumb({
+                decimals: 0,
+
+            }),
+        });
+
+        // Update Price Range
+        slider.noUiSlider.on("update", function (values, handle) {
+            console.log(values);
+            $("#filter-price-range").text(values.join(" - "));
+            $("#max").val(values[1]);
+            $("#min").val(values[0]);
+        });
+</script>
 @endsection
