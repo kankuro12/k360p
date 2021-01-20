bf63c3d75f9a96bc298792094db5c0b2d2c95134
kankuro12 <cms111000111@gmail.com>
2021-01-21 01:20:55 +0545

    wrapper changed

--- resources/views/themes/molla/elements/wrapper.blade.php
+++ resources/views/themes/molla/elements/wrapper.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     @foreach (App\model\admin\HomePageSection::where('parent_id', $data->id)->get() as $item_1)
-        <div class="col-md-{{ $item_1->row }} order-md-{{ $item_1->order }} pt-1" id="section_{{ $item_1->id }}">
+        <div class="col-md-{{ $item_1->row }} order-md-{{ $item_1->order }} pt-md-1" id="section_{{ $item_1->id }}">
             @if ($item_1->boxed == 1)
                 <div style="margin-left:2rem;margin-right:2rem">
                     @include($item_1->render() ,['data'=>$item_1])
